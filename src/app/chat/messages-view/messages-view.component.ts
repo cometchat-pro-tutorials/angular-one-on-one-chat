93bd7ab4b1c51022137380321982d1783ecd8f99
Adrian Fâciu <adrian.faciu@gmail.com>
2019-09-18 11:10:40 +0300

    Fixed scroll into view. Removed unused file.

--- src/app/chat/messages-view/messages-view.component.ts
+++ src/app/chat/messages-view/messages-view.component.ts
@@ -1,21 +1,47 @@
-import { Component, Output, EventEmitter, Input } from '@angular/core';
+import {
+  Component,
+  Output,
+  EventEmitter,
+  Input,
+  ViewChild,
+  ElementRef,
+  OnChanges,
+  SimpleChanges
+} from '@angular/core';
 import { AuthService } from 'src/app/core/auth.service';
 import { CometChat } from '@cometchat-pro/chat';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-messages-view',
   templateUrl: './messages-view.component.html',
   styleUrls: ['./messages-view.component.scss']
 })
-export class MessagesViewComponent {
+export class MessagesViewComponent implements OnChanges {
   @Input() messages: CometChat.TextMessage[] | null;
   @Input() selectedUserName: string;
 
   @Output() sendMessage = new EventEmitter<string>();
 
+  @ViewChild('scrollMe', { static: false })
+  messagesContainer: ElementRef<HTMLDivElement>;
+
   constructor(readonly authService: AuthService) {}
 
   onSendMessage(message: string) {
     this.sendMessage.emit(message);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.messages) {
+      timer(10).subscribe(() => this.scrollIntoView());
+    }
+  }
+
+  private scrollIntoView() {
+    if (this.messagesContainer) {
+      const { nativeElement } = this.messagesContainer;
+      nativeElement.scrollTop = nativeElement.scrollHeight;
+    }
   }
 }
